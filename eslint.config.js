4d9d185eab690ece08dda0b00a77223e6bd0c277
cgasgarth <64235119+cgasgarth@users.noreply.github.com>
2026-06-12 05:05:58 -0500

    tooling: enable unsafe call lint

--- eslint.config.js
+++ eslint.config.js
@@ -197,7 +197,7 @@ module.exports = [
       '@typescript-eslint/no-deprecated': 'error',
       '@typescript-eslint/use-unknown-in-catch-callback-variable': 'off',
       '@typescript-eslint/no-unnecessary-type-conversion': 'error',
-      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-call': 'error',
       '@typescript-eslint/no-useless-default-assignment': 'error',
       '@typescript-eslint/no-unsafe-return': 'error',
       '@typescript-eslint/no-unnecessary-type-arguments': 'error',
