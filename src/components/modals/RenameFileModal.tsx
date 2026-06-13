2d01525aa9b54ebc86a4c3151b3df6326eef3f59
Connor Gasgarth <64235119+cgasgarth@users.noreply.github.com>
2026-06-12 20:29:21 -0500

    a11y: mark passive modal backdrops presentational (#839)

--- src/components/modals/RenameFileModal.tsx
+++ src/components/modals/RenameFileModal.tsx
@@ -109,6 +109,7 @@ export default function RenameFileModal({ filesToRename, isOpen, onClose, onSave
         }
       }}
       onKeyDown={handleKeyDown}
+      role="presentation"
     >
       <div
         aria-modal="true"
