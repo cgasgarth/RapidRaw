4ef7412258a95f885e981a903e2ef4641f9f78f2
Connor Gasgarth <64235119+cgasgarth@users.noreply.github.com>
2026-06-15 11:48:04 -0500

    color(profile): add profile tone controls (#1231)

---
+++ src/utils/profileTonePresets.ts
@@ -0,0 +1,49 @@
+import { type ParametricCurveSettings } from './adjustments';
+import { type ToneCurveId } from '../schemas/profileToneSchemas';
+
+const BASE_PARAMETRIC_CURVE: ParametricCurveSettings = {
+  blackLevel: 0,
+  darks: 0,
+  highlights: 0,
+  lights: 0,
+  shadows: 0,
+  split1: 25,
+  split2: 50,
+  split3: 75,
+  whiteLevel: 0,
+};
+
+export const TONE_CURVE_PARAMETRIC_PRESETS: Record<ToneCurveId, ParametricCurveSettings> = {
+  auto_filmic: {
+    ...BASE_PARAMETRIC_CURVE,
+    darks: -4,
+    highlights: -8,
+    lights: 5,
+    shadows: 5,
+    split1: 22,
+    split3: 78,
+  },
+  high_contrast: {
+    ...BASE_PARAMETRIC_CURVE,
+    blackLevel: -2,
+    darks: -8,
+    highlights: 8,
+    lights: 8,
+    shadows: -6,
+    whiteLevel: 2,
+  },
+  linear: BASE_PARAMETRIC_CURVE,
+  shadow_lift: {
+    ...BASE_PARAMETRIC_CURVE,
+    darks: 8,
+    highlights: -4,
+    shadows: 12,
+  },
+  soft_contrast: {
+    ...BASE_PARAMETRIC_CURVE,
+    darks: -2,
+    highlights: -5,
+    lights: 4,
+    shadows: 4,
+  },
+};
