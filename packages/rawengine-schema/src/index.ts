9e5e064ce5393ff1663150fe4848acac9020e812
cgasgarth <64235119+cgasgarth@users.noreply.github.com>
2026-06-16 08:18:51 -0500

    feat(panorama): add synthetic stitch apply engine

--- packages/rawengine-schema/src/index.ts
+++ packages/rawengine-schema/src/index.ts
@@ -5,5 +5,6 @@ export * from './editCommandBus.js';
 export * from './hdrBracketDetection.js';
 export * from './hdrMergeApiTools.js';
 export * from './hdrMergeUiControls.js';
+export * from './panoramaSyntheticStitch.js';
 export * from './rawEngineSchemas.js';
 export * from './samplePayloads.js';
