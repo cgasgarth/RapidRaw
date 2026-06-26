420902a7b6dc4aceb89df59940060113bdd34199
cgasgarth <64235119+cgasgarth@users.noreply.github.com>
2026-06-26 04:35:21 -0500

    feat(agent): add color app-server tool

---
+++ src/utils/agentColorRecipe.ts
@@ -0,0 +1,21 @@
+import { stableAgentPreviewHash } from './agentPreviewEnvelope';
+
+import type { Adjustments } from './adjustments';
+
+export const buildAgentColorRecipeHashInput = (adjustments: Adjustments) => ({
+  blackWhiteMixer: adjustments.blackWhiteMixer,
+  channelMixer: adjustments.channelMixer,
+  colorBalanceRgb: adjustments.colorBalanceRgb,
+  colorCalibration: adjustments.colorCalibration,
+  colorGrading: adjustments.colorGrading,
+  hsl: adjustments.hsl,
+  saturation: adjustments.saturation,
+  selectiveColorRangeControls: adjustments.selectiveColorRangeControls,
+  skinToneUniformity: adjustments.skinToneUniformity,
+  temperature: adjustments.temperature,
+  tint: adjustments.tint,
+  vibrance: adjustments.vibrance,
+});
+
+export const hashAgentColorRecipeInput = (adjustments: Adjustments): string =>
+  stableAgentPreviewHash(JSON.stringify(buildAgentColorRecipeHashInput(adjustments)));
