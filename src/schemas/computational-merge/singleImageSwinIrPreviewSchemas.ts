fa4a04f1b07a352f68c3f768bc3738c571e95779
Connor Gasgarth <64235119+cgasgarth@users.noreply.github.com>
2026-07-10 21:02:06 -0500

    schema(sr): expose single-image preview response

---
+++ src/schemas/computational-merge/singleImageSwinIrPreviewSchemas.ts
@@ -0,0 +1,10 @@
+export {
+  singleImageSwinIrCapabilityIdV1,
+  singleImageSwinIrCapabilityV1Schema,
+  singleImageSwinIrPreviewPlanV1Schema,
+  singleImageSwinIrPreviewPublicationV1Schema,
+  singleImageSwinIrSourceModeV1,
+  type SingleImageSwinIrCapabilityV1,
+  type SingleImageSwinIrPreviewPlanV1,
+  type SingleImageSwinIrPreviewPublicationV1,
+} from '../../../packages/rawengine-schema/src/super-resolution/singleImageSwinIrPreviewSchemas';
